0d33e6a2042f585a3241fe4a409c810836bd3fc1
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-02-17 08:29:28 -0300

    aula 15

--- aula12.js
+++ aula12.js
@@ -23,7 +23,7 @@ function repetem(arr) {
     counts[ element ] = (counts[ element ] || 0) + 1; // tem esse retorno no couts { '1': 2, '2': 3, '3': 3, '4': 5, '5': 5 }
   });
 const valorMaximo = Math.max(...Object.keys(counts))// o spred(...) pega o que tem dentro de counts e espalha para 
-  console.log(Object.values(counts))
+  //console.log(Object.values(counts))
 
   const numero = Object.keys(counts).find((key) =>{
    return counts[key] === valorMaximo
@@ -32,5 +32,29 @@ return numero
 
 }
 // const numero = Object.keys(counts).find(key => {// como esta trazendo todas as chaves então busca uma em espacifico. o FIND procura uma so
+//console.log(repetem([2,4,4,1,1,2,2,3,3,3,3,3,4,4,4,5,5,5,5,5,]))
 
-console.log(repetem([2,4,4,1,1,2,2,3,3,3,3,3,4,4,4,5,5,5,5,5,]))
+
+//3 ##################################################################################
+//par ou impar
+/*
+let numero = 11;
+(numero % 2 === 0) ? console.log( ` o numero ${numero} e PAR` ) : console.log(` o Numero e Impar ${numero}`)
+*/
+
+//3 ######################################################################################
+
+// peso ideal 
+// altura  * 22 homens
+// altura  * 21 mulheres
+let altura = 1.80;
+let sexo = 'masculino';
+let pesoIdeal = 0;
+
+if (sexo === 'masculino') {
+  pesoIdeal = 22 * Math.pow(altura, 2)// pow e uma potencia
+} else {
+  pesoIdeal = 21 * Math.pow(altura, 2);
+}
+
+console.log(`Seu peso ideal e : ${pesoIdeal.toFixed(3)}Kg `)// tofixed e casas decimais 
